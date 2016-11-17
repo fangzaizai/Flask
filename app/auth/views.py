4c0710a74b37d5c8a6d4d930b9386672b26695cc
fangzaizai <anmeifang111@sina,com>
2016-11-17 19:29:05 +0800

    add register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from flask_login import login_user,logout_user, login_required
+from flask_login import login_user,logout_user, login_required, current_user
+import flask_login.utils
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from app import db
+from ..email import send_email
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,6 +15,7 @@ def login():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user, form.remember_me.data)
+			#把原地址保存在查询字符串的next参数中
 			return redirect(request.args.get('next') or url_for('main.index'))
 		flash('Invalid username or password')
 	return render_template('auth/login.html', form=form)
@@ -22,3 +26,20 @@ def logout():
 	logout_user()
 	flash('You have been logged out.')
 	return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	#验证通过
+	if form.validate_on_submit():
+		user = User(email=form.email.data,
+					username=form.username.data,
+					password=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		token = generate_confirmation_token()
+		send_email(user.email,"Confirm Your Account",'auth/email/confirm',user=user, token=token)
+		flash('confirm mail has sent')
+		return redirect(url_for('main.index'))
+	return render_template('auth/register.html', form=form)
+
--- app/models.py
+++ app/models.py
@@ -3,22 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
-@login_manager.user_loader
-def load_user(user_id):
-	#如果找到用户，返回用户对象，否则返回None
-	return User.query.get(int(user_id))  
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
 	users = db.relationship('User', backref='role', lazy='dynamic')  #?
+	confirmed = db.Column(db.Boolean, default=False)
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	"""docstring for User"""
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
@@ -26,9 +24,9 @@ class User(db.Model):
 	username = db.Column(db.String(64), unique=True, index=True)
 	password_hash = db.Column(db.String(128))
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-	
-	def __repr__(self):
-		return '<User %r>' % self.name
+	#用于将对象打印出来，将属性和方法整理成一个可以打印输出的格式
+	#def __repr__(self):
+		#return '<User %r>' % self.username
 
 	@property
 	def password(self):
@@ -40,3 +38,26 @@ class User(db.Model):
 
 	def verify_password(self, password):
 		return check_password_hash(self.password_hash, password)
+
+	#生成一个；令牌
+	def generate_confirmation_token(self, expiration=3600):
+		s = Serializer(current_app.config['SECRET_KEY'], expiration)
+		return s.dump({'confirm': self.id})
+
+	#检验令牌
+	def confirm(self, token):
+		s=Serializer(current_app.config['SECRET_KEY'])
+		try:
+			data=s.loads(token)
+		except:
+			return False
+		if data.get('confirm') != self.id:
+			return False
+		self.confirmed = True
+		db.session.add(self)
+		return True
+
+@login_manager.user_loader
+def load_user(user_id):
+	#如果找到用户，返回用户对象，否则返回None
+	return User.query.get(int(user_id))  
